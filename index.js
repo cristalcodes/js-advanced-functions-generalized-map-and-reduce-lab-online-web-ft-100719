889da3fd80e60ab12817076b9a4f0bd2ddbdb50e
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-02-06 15:20:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(sourceArr,fn) {
+  let results = []
+  for (let i=0; i < sourceArr.length; i++){
+    results.push(fn(sourceArr[i]))
+  }
+  return results
+}
+
+function reduce(sourceArr, fn, startingPoint = 0){
+  let total = (!!startingPoint) ? startingPoint : sourceArr[0]
+  let i = (!!startingPoint) ? 0: 1
+  for ( ;i < sourceArr.length; i++){
+    total = fn(sourceArr[i], total)
+  }
+  return total
+}
